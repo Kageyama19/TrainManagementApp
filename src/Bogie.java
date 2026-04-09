dc6dea848a26914819a7cdfce396da98d1d80590
Kageyama19 <tc4369@srmist.edu.in>
2026-04-09 09:35:48 +0530

    Make Bogie compatible across UC7 and UC8

--- src/Bogie.java
+++ src/Bogie.java
@@ -1,14 +1,28 @@
 public class Bogie {
-    private final String bogieName;
+    private final String bogieId;
+    private final String bogieType;
     private final int capacity;
 
     public Bogie(String bogieName, int capacity) {
-        this.bogieName = bogieName;
+        this(null, bogieName, capacity);
+    }
+
+    public Bogie(String bogieId, String bogieType, int capacity) {
+        this.bogieId = bogieId;
+        this.bogieType = bogieType;
         this.capacity = capacity;
     }
 
+    public String getBogieId() {
+        return bogieId;
+    }
+
+    public String getBogieType() {
+        return bogieType;
+    }
+
     public String getBogieName() {
-        return bogieName;
+        return bogieType;
     }
 
     public int getCapacity() {
@@ -17,8 +31,16 @@ public class Bogie {
 
     @Override
     public String toString() {
+        if (bogieId == null) {
+            return "Bogie{" +
+                    "bogieName='" + bogieType + '\'' +
+                    ", capacity=" + capacity +
+                    '}';
+        }
+
         return "Bogie{" +
-                "bogieName='" + bogieName + '\'' +
+                "bogieId='" + bogieId + '\'' +
+                ", bogieType='" + bogieType + '\'' +
                 ", capacity=" + capacity +
                 '}';
     }
