2c2191c1444b6e5b40bad15c132907edea730a79
Kageyama19 <tc4369@srmist.edu.in>
2026-04-09 09:44:59 +0530

    Align UC9 Bogie model with main

--- src/Bogie.java
+++ src/Bogie.java
@@ -3,10 +3,6 @@ public class Bogie {
     private final String bogieType;
     private final int capacity;
 
-    public Bogie(String bogieName, int capacity) {
-        this(null, bogieName, capacity);
-    }
-
     public Bogie(String bogieId, String bogieType, int capacity) {
         this.bogieId = bogieId;
         this.bogieType = bogieType;
@@ -21,23 +17,12 @@ public class Bogie {
         return bogieType;
     }
 
-    public String getBogieName() {
-        return bogieType;
-    }
-
     public int getCapacity() {
         return capacity;
     }
 
     @Override
     public String toString() {
-        if (bogieId == null) {
-            return "Bogie{" +
-                    "bogieName='" + bogieType + '\'' +
-                    ", capacity=" + capacity +
-                    '}';
-        }
-
         return "Bogie{" +
                 "bogieId='" + bogieId + '\'' +
                 ", bogieType='" + bogieType + '\'' +
